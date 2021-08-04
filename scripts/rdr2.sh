8e2a4929116c4f85b02cb0d8a04f262ff09f491c
guido <guido@fractal-debian.rexinet.nl>
2021-08-04 08:51:56 +0200

    Eerste commit

---
+++ scripts/rdr2.sh
@@ -0,0 +1,2 @@
+cp /home/guido/Onedrive/Commands/imports/RDR2.cmd /home/guido/Onedrive/Commands/imports/W10Gaming.cmd
+sudo virsh start --domain W10Gaming
---
+++ scripts/switchback.sh
@@ -0,0 +1,1 @@
+ssh Guido@192.168.100.101 c:/\scripts/\switchback.cmd
